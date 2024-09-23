5868a2c760c9807f7cb4a096eb0392b3e96e2d6c
신승열 <shinseungyeol0227@gmail.com>
2024-09-23 22:24:41 +0900

    :sparkles:  PolicyCheckerTest 사용 정책 체크 테스트 추가 및 테스트 코드 리팩토링
    - 보유 포인트 초과로 사용할 수 없다
    - 0 이하로 포인트 사용할 수 없다

--- src/test/java/io/hhplus/tdd/point/PolicyCheckerTest.java
+++ src/test/java/io/hhplus/tdd/point/PolicyCheckerTest.java
@@ -30,7 +30,34 @@ class PolicyCheckerTest {
         });
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            policyChecker.checkChargePolicy(-1, 0);
+            policyChecker.checkChargePolicy(0, -1);
         });
+    }
+
+    /**
+     * 사용 정책중 사용하려는 포인트가 부족하면 안되는 정책 단위 테스트 함수
+     */
+    @Test
+    public void 포인트_사용은_보유한_포인트_초과로_사용할_수_없다() {
+        long amountToUse = 100, remainingAmount = 10;
+
+        Assertions.assertThrows(IllegalStateException.class, () -> {
+            policyChecker.checkUsePolicy(remainingAmount, amountToUse);
+        });
+    }
+
+    /**
+     * 사용 정책중, 사용하려는 포인트가 0 이하면 안되는 정책 단위 테스트 함수
+     */
+    @Test
+    public void 사용하려는_포인트가_0_이하면_안된다() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            policyChecker.checkChargePolicy(0, 0);
+        });
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            policyChecker.checkChargePolicy(0, -1);
+        });
+
     }
 }
