4b787a639f57f19db0af5fd9eea838c7500a8fce
신승열 <shinseungyeol0227@gmail.com>
2024-09-28 09:38:18 +0900

    readme.md 추가

--- src/test/java/io/hhplus/tdd/point/service/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/point/service/PointServiceIntegrationTest.java
@@ -48,4 +48,34 @@ public class PointServiceIntegrationTest {
         UserPoint finalUserPoint = pointService.searchUserPoint(userId);
         Assertions.assertEquals(0, finalUserPoint.point());
     }
+
+    @Test
+    void 동시성_사용_충전_테스트() throws InterruptedException {
+        long userId = 1L;
+        long initialAmount = 10;
+
+        pointService.chargeUserPoint(userId, initialAmount);
+
+        int numberOfThreads = 30;
+        CountDownLatch latch = new CountDownLatch(numberOfThreads);
+        Thread[] threads = new Thread[numberOfThreads];
+
+        for (int i = 0; i < numberOfThreads; i++) {
+            threads[i] = new Thread(() -> {
+                try {
+                    // 포인트 사용 또는 충전
+                    pointService.useUserPoint(userId, 1);
+                } finally {
+                    latch.countDown(); // 스레드 종료 시 카운트 감소
+                }
+            });
+            threads[i].start();
+        }
+        // 모든 스레드가 종료될 때까지 대기
+        latch.await();
+
+        // 최종 포인트 조회
+        UserPoint finalUserPoint = pointService.searchUserPoint(userId);
+        Assertions.assertEquals(0, finalUserPoint.point());
+    }
 }
